4240626bb5a35a8d6c7e4b8d0409d50536289cac
iganna <igolkinaanna11@gmail.com>
2018-01-27 01:53:54 +0300

    intersect final

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from lspec_db import LspecDatabase
 
 
-path_to_db = 'data_soil/pure_ref/dsgn05/'
-d = LspecDatabase(path_to_db)
-
-d.intersection()
+n_ref_samples = ['05', '10', '15', '20', '25', '29']
+for s in n_ref_samples:
+    path_to_db = 'data_soil/pure_ref/dsgn' + s + '/'
+    path_to_ugene = 'ugene-spb/'
+    d = LspecDatabase(path_to_db)
+    d.intersection(int(s))
 
 
