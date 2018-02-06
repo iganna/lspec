3f624fc7341d3d3fa8b833d1e698fd1bf74f9b0b
iganna <igolkinaanna11@gmail.com>
2018-02-06 20:59:30 +0300

    simulation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from lspec_db import LspecDatabase
+from lspec_signals import LspecSignals
 
 #
 # n_ref_samples = ['05', '10', '15', '20', '25', '29']
@@ -7,10 +8,16 @@ from lspec_db import LspecDatabase
 #
 #
 
-s = '00'
-path_to_db = 'data_soil/pure_ref/dsgn' + s + '/'
-path_to_ugene = 'ugene-spb/'
-d = LspecDatabase(path_to_db)
-d.intersection(29)
+# s = '00'
+# path_to_db = 'data_soil/pure_ref/dsgn' + s + '/'
+# path_to_ugene = 'ugene-spb/'
+# d = LspecDatabase(path_to_db)
+# d.intersection(29)
+
+
+path_to_lspec = 'data_simulation/out_13/'
+path_to_test = 'data_simulation/test/'
+signals = LspecSignals(path_to_lspec, path_to_test)
+signals.intersect()
 
 
