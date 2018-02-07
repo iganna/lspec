922564a954fbd838a6fa79c2d9fefe4efa280f7a
iganna <igolkinaanna11@gmail.com>
2018-02-07 19:53:54 +0300

    artificial

--- main.py
+++ main.py
@@ -2,26 +2,23 @@ from lspec_db import LspecDatabase
 from lspec_signals import LspecSignals
 
 
-# n_ref_samples = ['05', '10', '15', '20', '25', '29']
-# n_ref_samples = ['00']
-# for s in n_ref_samples:
+
+
+n_ref_samples = ['01', '02', '03', '04', '05']
+for s in n_ref_samples:
+    path_to_db = 'data_simulation/norm01/design_' + s + '/'
+    path_to_ugene = 'ugene-spb/'
+    d = LspecDatabase(path_to_db)
+    d.intersection(20)
+
+
+
 #
 #
-
-# s = '00'
-# path_to_db = 'data_soil/pure_ref/dsgn' + s + '/'
-# path_to_ugene = 'ugene-spb/'
-# d = LspecDatabase(path_to_db)
-# d.intersection(29)
-
-
-
-
-
-path_to_lspec = 'data_simulation/lspecs/'
-path_to_test = 'data_simulation/test/'
-signals = LspecSignals(path_to_lspec, path_to_test)
-# signals.intersect()
-signals.intersect_approxim(29)
+# path_to_lspec = 'data_simulation/lspecs/'
+# path_to_test = 'data_simulation/test/'
+# signals = LspecSignals(path_to_lspec, path_to_test)
+# # signals.intersect()
+# signals.intersect_approxim(29)
 
 
