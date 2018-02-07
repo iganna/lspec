0e5ace753a3bcc9d243ded353a5a07bb7bc09307
iganna <igolkinaanna11@gmail.com>
2018-02-07 23:01:33 +0300

    artificial

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ from lspec_signals import LspecSignals
 
 
 
+#
+# n_ref_samples = ['01', '02', '03', '04', '05']
+# for s in n_ref_samples:
+#     path_to_db = 'data_simulation/norm01/design_' + s + '/'
+#     path_to_ugene = 'ugene-spb/'
+#     d = LspecDatabase(path_to_db)
+#     d.intersection(20)
+
+
 
 n_ref_samples = ['01', '02', '03', '04', '05']
 for s in n_ref_samples:
-    path_to_db = 'data_simulation/norm01/design_' + s + '/'
-    path_to_ugene = 'ugene-spb/'
-    d = LspecDatabase(path_to_db)
-    d.intersection(20)
+    path_to_lspec = 'data_simulation/norm01/design_' + s + '/' + 'lspecs/'
+    path_to_test = 'data_simulation/norm01/a_artificial/'
+    path_to_signals = path_to_test + 'result_' + s + '/'
+    signals = LspecSignals(path_to_lspec, path_to_test, path_to_signals)
+    # signals.intersect()
+    signals.intersect_approxim(30)
 
 
 
-#
-#
-# path_to_lspec = 'data_simulation/lspecs/'
-# path_to_test = 'data_simulation/test/'
-# signals = LspecSignals(path_to_lspec, path_to_test)
-# # signals.intersect()
-# signals.intersect_approxim(29)
 
 
