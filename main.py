02d6ac8bc34b1e8fc47ca53c707d24eb7c804d3c
iganna <igolkinaanna11@gmail.com>
2018-02-08 12:27:14 +0300

    art

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ from lspec_signals import LspecSignals
 #     d.intersection(20)
 
 
-
-n_ref_samples = ['01', '02', '03', '04', '05']
-for s in n_ref_samples:
-    path_to_lspec = 'data_simulation/norm01/design_' + s + '/' + 'lspecs/'
-    path_to_test = 'data_simulation/norm01/a_artificial/'
-    path_to_signals = path_to_test + 'result_' + s + '/'
-    signals = LspecSignals(path_to_lspec, path_to_test, path_to_signals)
-    # signals.intersect()
-    signals.intersect_approxim(30)
+#
+# n_ref_samples = ['01', '02', '03', '04', '05']
+# for s in n_ref_samples:
+#     path_to_lspec = 'data_simulation/norm01/design_' + s + '/' + 'lspecs/'
+#     path_to_test = 'data_simulation/norm01/a_artificial/'
+#     path_to_signals = path_to_test + 'result_' + s + '/'
+#     signals = LspecSignals(path_to_lspec, path_to_test, path_to_signals)
+#     # signals.intersect()
+#     signals.intersect_approxim(30)
 
 n_ref_samples = ['01', '02', '03', '04', '05']
 for s in n_ref_samples:
